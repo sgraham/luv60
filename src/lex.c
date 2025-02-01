f0999783219277e54291b3c2c9a7ee70a8b32194
Scott Graham <scott.github@h4ck3r.net>
2025-01-31 19:10:20 -0800

    allow ` in indentifierish to work with float hack for now

--- src/lex.c
+++ src/lex.c
@@ -461,7 +461,7 @@ uint32_t lex_indexer_fallback(const uint8_t* buf,
       ['e'] = true, ['f'] = true, ['g'] = true, ['h'] = true, ['i'] = true, ['j'] = true,
       ['k'] = true, ['l'] = true, ['m'] = true, ['n'] = true, ['o'] = true, ['p'] = true,
       ['q'] = true, ['r'] = true, ['s'] = true, ['t'] = true, ['u'] = true, ['v'] = true,
-      ['w'] = true, ['x'] = true, ['y'] = true, ['z'] = true,
+      ['w'] = true, ['x'] = true, ['y'] = true, ['z'] = true, ['`'] = true,
   };
 
 #if 0
